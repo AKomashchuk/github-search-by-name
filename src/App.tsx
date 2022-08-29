8f667d1ada22880e4f61390662ac0e504f117c0d
Alina Komashchuk <akomashchuk24@gmail.com>
2022-08-29 20:16:14 +0300

    add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { getSuggestedQuery } from '@testing-library/react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [query, setQuery] = useState('');
+
+  const onSubmit = (event: any) => {
+    event.preventDefaut();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1>Serch reposetories by name</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder='"Search"' value={query} onChange={(event) => setQuery(event.target.value)}/>
+        <button>Done</button>
+      </form>
+    </header>
   );
 }
 
